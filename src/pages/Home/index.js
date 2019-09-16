e3ea94cd4ee17631f0fe3ec461be9c2998fec535
Jonathan Garrido <jonathan.garrido1988@gmail.com>
2019-09-16 12:39:20 -0300

    feat(home): componente listo (estilos y acciones)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Typography, Card, Grid, TextField, Button } from '@material-ui/core'
 
-function Home() {
+import styles from './styles'
+import { MovieIcon } from '../../icons'
+
+function Home({ history }) {
+ 
+  const [search, setSearch] = useState('')
+  const classes = styles()
+
+  const onSearch = e => {
+    history.push(`/results?movieName=${search}`)
+  }
+
   return (
-    <div>
-      Home
-    </div>
+    <Container className={classes.container}>
+      <Card className={classes.cardContainer}>
+        <Grid container className={classes.titleGridContainer}>
+          <Grid>
+            <Typography className={classes.title}>Bienvenido!</Typography>
+          </Grid>
+          <Grid>
+            <MovieIcon className={classes.movieIcon}/>
+          </Grid>
+        </Grid>
+        <TextField 
+          value={search}
+          placeholder='Buscar...'
+          className={classes.textFieldSearch}
+          onChange={e=>setSearch(e.target.value)}
+        />
+        <Grid className={classes.buttonsContainer}>
+          <Button variant='contained' onClick={()=>setSearch('')} >Limpiar</Button>
+          <Button variant='contained' color='primary' size='large' className={classes.searchButton} onClick={onSearch} >Buscar</Button>
+        </Grid>
+      </Card>
+    </Container>
   )
 }
 
